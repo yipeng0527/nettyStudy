aff557318d2d772b02dde2e43f41533021052536
ex-yipeng <ex-yipeng@htlic.com>
2020-08-04 13:11:29 +0800

    新增websocket代码

---
+++ src/main/java/com/yp/netty/websocket/MyServer.java
@@ -0,0 +1,71 @@
+package com.yp.netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketClientProtocolHandler;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author ex-yipeng
+ * @version Id: MyServer.java, v 0.1 2020/8/4 9:10 ex-yipeng Exp $
+ */
+public class MyServer {
+    public static void main(String[] args) throws Exception {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            //因为基于http协议，使用http的编码和解码器
+                            pipeline.addLast(new HttpServerCodec());
+                            //是以块方式写，添加ChunkedWriteHandler处理器
+                            pipeline.addLast(new ChunkedWriteHandler());
+                              /*
+                                说明
+                                1. http数据在传输过程中是分段, HttpObjectAggregator ，就是可以将多个段聚合
+                                2. 这就就是为什么，当浏览器发送大量数据时，就会发出多次http请求
+                              */
+                            pipeline.addLast(new HttpObjectAggregator(8192));
+                              /*
+                                说明
+                                1. 对应websocket ，它的数据是以 帧(frame) 形式传递
+                                2. 可以看到WebSocketFrame 下面有六个子类
+                                3. 浏览器请求时 ws://localhost:8888/hello 表示请求的uri
+                                4. WebSocketServerProtocolHandler 核心功能是将 http协议升级为 ws协议 , 保持长连接
+                                5. 是通过一个 状态码 101
+                              */
+                            pipeline.addLast(new WebSocketServerProtocolHandler("/hello"));
+
+                            pipeline.addLast(new MyTextWebSocketFrameHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = server.bind(8888).sync();
+            System.out.println("服务器启动ok.....");
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
