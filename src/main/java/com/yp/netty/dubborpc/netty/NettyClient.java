1e92cc8264c7a3902693bed1c80dabd7effceee7
yipeng0527 <511580331@qq.com>
2020-08-18 11:01:07 +0800

    去掉关闭代码

--- src/main/java/com/yp/netty/dubborpc/netty/NettyClient.java
+++ src/main/java/com/yp/netty/dubborpc/netty/NettyClient.java
@@ -1,10 +1,7 @@
 package com.yp.netty.dubborpc.netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -19,47 +16,44 @@ import java.util.concurrent.Executors;
  */
 public class NettyClient {
 
-    private static ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private static ExecutorService executor =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     private int count = 0;
 
     private static NettyClientHandler clientHandler;
 
     public Object getBean(final Class<?> serivceClass, final String providerName) {
-        return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{serivceClass}, ((proxy, method, args) -> {
-            if (null == clientHandler) {
-                initClient();
-            }
-            clientHandler.setPara(providerName + args[0]);
-            return executor.submit(clientHandler).get();
-        }));
+        return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class<?>[]{serivceClass},
+                (proxy, method, args) -> {
+                    if (null == clientHandler) {
+                        initClient();
+                    }
+                    clientHandler.setPara(providerName + args[0]);
+                    return executor.submit(clientHandler).get();
+                });
     }
 
     private static void initClient() {
         clientHandler = new NettyClientHandler();
         EventLoopGroup group = new NioEventLoopGroup();
-
+        Bootstrap client = new Bootstrap();
+        client.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new StringDecoder());
+                        pipeline.addLast(new StringEncoder());
+                        pipeline.addLast(clientHandler);
+                    }
+                });
         try {
-            Bootstrap client = new Bootstrap();
-
-            client.group(group)
-                    .channel(NioSocketChannel.class)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            ChannelPipeline pipeline = socketChannel.pipeline();
-                            pipeline.addLast(new StringDecoder());
-                            pipeline.addLast(new StringEncoder());
-                            pipeline.addLast(clientHandler);
-                        }
-                    });
-
-            ChannelFuture channelFuture = client.connect("127.0.0.1", 8888).sync();
-            //channelFuture.channel().closeFuture().sync();
+            client.connect("127.0.0.1", 8888).sync();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            group.shutdownGracefully();
         }
     }
 }
